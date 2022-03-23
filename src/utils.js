a673d22ba56d08ba39825edfd01e35dca73e192e
hritik <86347286+hritikd3@users.noreply.github.com>
2022-03-23 23:37:50 +0530

    worked on paginate

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,12 @@ const paginate = (followers) => {
   // console.log(followers);
   const itemsPerPage = 12;
   const pages = Math.ceil(followers.length / itemsPerPage);
-  console.log(pages);
+  // console.log(pages);
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemsPerPage;
+    return followers.slice(start, start + itemsPerPage);
+  });
+  console.log(newFollowers);
 };
 
 export default paginate;
