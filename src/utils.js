34749b078b629f174560d93729b4b8a9cffe3371
hritik <86347286+hritikd3@users.noreply.github.com>
2022-03-23 18:52:59 +0530

    added functionality for container appear to be only 12

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,8 @@
 const paginate = (followers) => {
-  console.log();
+  // console.log(followers);
+  const itemsPerPage = 12;
+  const pages = Math.ceil(followers.length / itemsPerPage);
+  console.log(pages);
 };
 
 export default paginate;
