bd4c9516b759dfd39ab2e02b4e3ae2e4d5bc63b7
hritik <86347286+hritikd3@users.noreply.github.com>
2022-03-23 23:54:32 +0530

    paginate component completed

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,15 @@
 const paginate = (followers) => {
   // console.log(followers);
-  const itemsPerPage = 12;
+  const itemsPerPage = 10;
   const pages = Math.ceil(followers.length / itemsPerPage);
   // console.log(pages);
   const newFollowers = Array.from({ length: pages }, (_, index) => {
     const start = index * itemsPerPage;
+    console.log(start);
     return followers.slice(start, start + itemsPerPage);
   });
-  console.log(newFollowers);
+  // console.log(newFollowers);
+  return newFollowers;
 };
 
 export default paginate;
