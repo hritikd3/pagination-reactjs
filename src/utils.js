60bf40fdadb60daeb0a850a0f9bb2a2ad2802b2a
hritik <86347286+hritikd3@users.noreply.github.com>
2022-03-25 14:22:24 +0530

    done

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,18 @@
 const paginate = (followers) => {
   // console.log(followers);
+<<<<<<< CodeSandbox
+  const itemsPerPage=9
+};
+
+export default paginate;
+
+=======
   const itemsPerPage = 10;
   const pages = Math.ceil(followers.length / itemsPerPage);
   // console.log(pages);
   const newFollowers = Array.from({ length: pages }, (_, index) => {
     const start = index * itemsPerPage;
+>>>>>>> GitHub
     console.log(start);
     return followers.slice(start, start + itemsPerPage);
   });
